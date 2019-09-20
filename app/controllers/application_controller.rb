96ae5e277c435992f182f9ddd00e213730d5a70f
Ranika Williams <rnkprice07@gmail.com>
2019-09-20 17:14:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    if params[:username].empty
+    if params[:username].empty?
       redirect to '/failure'
     end
       
